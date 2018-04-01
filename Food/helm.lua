c1f1cf908e1a624874dd410d82c3eb2244608500
Matthew Fisher <matt.fisher@microsoft.com>
2018-04-01 03:13:35 -0700

    add Helm

---
+++ Food/helm.lua
@@ -0,0 +1,32 @@
+local name = "helm"
+local version = "2.8.2"
+
+food = {
+    name = name,
+    description = "The Kubernetes Package Manager",
+    homepage = "https://github.com/kubernetes/helm",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://storage.googleapis.com/kubernetes-helm/helm-v" .. version .. "-darwin-amd64.tar.gz",
+            sha256 = "a0a8cf462080b2bc391f38b7cf617618b189cdef9f071c06fa0068c2418cc413",
+            binpath = "darwin-amd64/" .. name
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://storage.googleapis.com/kubernetes-helm/helm-v" .. version .. "-linux-amd64.tar.gz",
+            sha256 = "614b5ac79de4336b37c9b26d528c6f2b94ee6ccacb94b0f4b8d9583a8dd122d3",
+            binpath = "linux-amd64/" .. name
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://storage.googleapis.com/kubernetes-helm/helm-v" .. version .. "-windows-amd64.tar.gz",
+            sha256 = "cb6ea5d60f202c752f1f0777e4bebd98c619a2c18e52468df7a302e783216f23",
+            binpath = "windows-amd64\\" .. name
+        }
+    }
+}
