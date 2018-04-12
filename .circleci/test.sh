9f610778f960653b5eb0cd1c7f2f7e7086cdb497
Matthew Fisher <matt.fisher@microsoft.com>
2018-04-11 20:53:54 -0700

    install the PR's branch as the default rig

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 IFS=$'\n\t'
 ROOT="${BASH_SOURCE[0]%/*}/.."
 
+eval $(gofish tank)
+rm -rf "$FISH_DEFAULT_RIG"
+cp -R "$ROOT" "$FISH_DEFAULT_RIG"
+
 cd "$ROOT"
 
 for i in $(ls Food/); do
