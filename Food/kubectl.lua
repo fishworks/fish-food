60dcf7ce2a43c1d77128502677198cafdcf61117
Matthew Fisher <matt.fisher@microsoft.com>
2018-04-25 22:38:56 -0700

    bump kubectl to 1.10.1

--- Food/kubectl.lua
+++ Food/kubectl.lua
@@ -1,5 +1,5 @@
 local name = "kubectl"
-local version = "0.10.0"
+local version = "1.10.1"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://storage.googleapis.com/kubernetes-release/release/v" .. version .. "/bin/darwin/amd64/kubectl",
-            sha256 = "05a966053691819c20e07e1afb8debe1e2edd2ab9c866ad6f0324d3cfe7a1c89",
+            sha256 = "9484fd8a0cba513ab91fc192f6b6659d700f411502d01a7e02acb9f3b986037c",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://storage.googleapis.com/kubernetes-release/release/v" .. version .. "/bin/linux/amd64/kubectl",
-            sha256 = "90127a228335ba932fa30c008028780fecfdc6e04d7fb399d331a098409fb8c6",
+            sha256 = "1bb4d3793fb0f9e1cfee86599e0f43ae5f15578a01b61011fe7c9488e114a00b",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://storage.googleapis.com/kubernetes-release/release/v" .. version .. "/bin/windows/amd64/kubectl.exe",
-            sha256 = "eae58c98d261aaa0b08bfd8705b3a10cb32331d4dbc333c015f47861d6af6a7b",
+            sha256 = "b3c36238038c08792fdb775dec6dba03361496e9e0774f829fdc1c8d5b2aebd4",
             resources = {
                 {
                     path = name .. ".exe",
