6bcd2df268f7d88cad69d067e0bd0b8e2b13e7d2
Matthew Fisher <matt.fisher@microsoft.com>
2018-05-12 01:09:29 +0100

    bump gofish to v0.6.1

--- Food/gofish.lua
+++ Food/gofish.lua
@@ -1,5 +1,5 @@
 local name = "gofish"
-local version = "0.6.0"
+local version = "0.6.1"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://gofi.sh/releases/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "f9fc27f0933348e8c5ab9b8463f6faa3452d8cc7de4c63ede17c7d0c85034eeb",
+            sha256 = "eeebf29ac9bf8b97f5312f327d4c917811fd01fb30bc0e0c7d0aaf54c24b4c4d",
             resources = {
                 {
                     path = "darwin-amd64/" .. name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://gofi.sh/releases/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "cb435f397d072fb39f453a75d4f4ff491b4fbd6a8672fd1f3cf77064d67fb5a9",
+            sha256 = "b461d4be56c152d359b0fe369fbfc110a99dbecc7dc9387594cc8e28b58145c9",
             resources = {
                 {
                     path = "linux-amd64/" .. name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://gofi.sh/releases/" .. name .. "-v" .. version .. "-windows-amd64.zip",
-            sha256 = "53485175905dae53528631e6b2107d7840d810bc8435781a500af9451c26f032",
+            sha256 = "842397bfdf0ea75e4a53998e2c629fb8ba65e6b1be4582ff28e31970fe2f7d6f",
             resources = {
                 {
                     path = "windows-amd64\\" .. name .. ".exe",
