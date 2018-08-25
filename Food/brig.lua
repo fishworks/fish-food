339cb592afff72d779b694350b62b3d5e18d76ef
Matthew Fisher <matt.fisher@microsoft.com>
2018-08-25 08:34:33 -0700

    fix shasums for brig

--- Food/brig.lua
+++ Food/brig.lua
@@ -29,7 +29,7 @@ food = {
             arch = "amd64",
             url = baseURL .. "/v" .. version .. "/" .. name .. "-linux-amd64",
             -- shasum of the release archive
-            sha256 = "104a8731414e59cfc9eb608f1bd289118a9aab7dc2d4a696a01fd212dcc0317c",
+            sha256 = "6429cfd02e637b1210f2ae10fb6b58df1524e1faa4522cb40ab506a432456197",
             resources = {
                 {
                     path = name .. "-linux-amd64",
@@ -43,7 +43,7 @@ food = {
             arch = "amd64",
             url = baseURL .. "/v" .. version .. "/" .. name .. "-windows-amd64.exe",
             -- shasum of the release archive
-            sha256 = "d5e43a9acbe5c2db8882ab01382a4035dd74c92c3f976f8d5d6f9539a9051507",
+            sha256 = "6d761a5a2af670b101846d488cfada632b27b5a2b93523b0c19b837396122599",
             resources = {
                 {
                     path = name .. "-windows-amd64.exe",
