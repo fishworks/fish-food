6c413257d7a91bb51d5f10abdeb979fd9501c412
Matthew Fisher <matt.fisher@microsoft.com>
2018-11-12 18:51:56 +0100

    fix shasums

--- Food/helm.lua
+++ Food/helm.lua
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://storage.googleapis.com/kubernetes-helm/helm-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "",
+            sha256 = "551b13a398749ae3e0a5c54d3078f6e3bee552c5d6a0bf6f338cab64ce38ab0f",
             resources = {
                 {
                     path = "darwin-amd64/" .. name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://storage.googleapis.com/kubernetes-helm/helm-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "",
+            sha256 = "02a4751586d6a80f6848b58e7f6bd6c973ffffadc52b4c06652db7def02773a1",
             resources = {
                 {
                     path = "linux-amd64/" .. name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://storage.googleapis.com/kubernetes-helm/helm-v" .. version .. "-windows-amd64.tar.gz",
-            sha256 = "",
+            sha256 = "8347dc5ea12d937eb793ed438ea3274f40639144abdc8b071715937aad8a7616",
             resources = {
                 {
                     path = "windows-amd64\\" .. name .. ".exe",
