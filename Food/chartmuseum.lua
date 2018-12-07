8f9c6b1fd492e4f983d2b2388d7c259429b7a3a7
Josh Dolitsky <jdolitsky@gmail.com>
2018-12-07 12:04:36 -0600

    chartmuseum 0.8.0 (#82)

--- Food/chartmuseum.lua
+++ Food/chartmuseum.lua
@@ -1,10 +1,10 @@
 local name = "chartmuseum"
-local version = "0.7.1"
+local version = "0.8.0"
 
 food = {
     name = name,
-    description = "The Helm Chart Repository server",
-    license = "MIT",
+    description = "Host your own Helm Chart Repository",
+    license = "Apache-2.0",
     homepage = "https://github.com/helm/chartmuseum",
     version = version,
     packages = {
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://s3.amazonaws.com/chartmuseum/release/v" .. version .. "/bin/darwin/amd64/chartmuseum",
-            sha256 = "d08a9bc0902bc2ab4b843d22b944b5c47db0959a6ba12db905711f2edc84c921",
+            sha256 = "0fe2677b999eb5eb714a42b699e6b956b73e6f3ab2bee5c705bf375cad85a075",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://s3.amazonaws.com/chartmuseum/release/v" .. version .. "/bin/linux/amd64/chartmuseum",
-            sha256 = "f6716b13796e8a01790ba819a654a0f3d7df9e7c47f5f64da8d9a57b738bafb1",
+            sha256 = "cba2cc4351db2da1c72552941405fde9050829b3a28695d9db3c7f02f29781bf",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://s3.amazonaws.com/chartmuseum/release/v" .. version .. "/bin/windows/amd64/chartmuseum",
-            sha256 = "3df68338657ddcd92a63ef8da7f464035f9425cf08004c23d5f7727993d85b8e",
+            sha256 = "88c2080f2d64b9a9dcdaf1495bdca86bd46685ff5a469dbbda8dc812d835ba47",
             resources = {
                 {
                     path = name,
