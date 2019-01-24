ce1ccad38a99f9bbf786abe2f63b1e3305b00cfb
Chase Hutchins <syntaqx@gmail.com>
2019-01-24 16:31:01 -0700

    copy paste fail, readd commas

--- Food/serve.lua
+++ Food/serve.lua
@@ -10,7 +10,7 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/syntaqx/serve/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_macos_x86_64.tar.gz"
+            url = "https://github.com/syntaqx/serve/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_macos_x86_64.tar.gz",
             sha256 = "95951376c655c512578de440a2f455cfb1b3a8fc20dc34ac6e9474eba87846ba",
             resources = {
                 {
@@ -23,7 +23,7 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/syntaqx/serve/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_linux_x86_64.tar.gz"
+            url = "https://github.com/syntaqx/serve/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_linux_x86_64.tar.gz",
             sha256 = "e66f59442252dcdc097e019aecb1426c71896a8135531271dd4fd0840a75e96c",
             resources = {
                 {
@@ -36,7 +36,7 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/syntaqx/serve/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_windows_x86_64.zip"
+            url = "https://github.com/syntaqx/serve/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_windows_x86_64.zip",
             sha256 = "d256f3b072227a969fb5ca93d3a946269e2d04e882ef2e033f0c4e0a4ab78baf",
             resources = {
                 {
