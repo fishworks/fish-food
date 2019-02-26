a24ea3db47c513c5e8487f16c246778308f3d47f
Matthew Fisher <matt.fisher@microsoft.com>
2019-02-26 10:57:07 -0800

    chartmuseum 0.8.2
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- Food/chartmuseum.lua
+++ Food/chartmuseum.lua
@@ -1,5 +1,5 @@
 local name = "chartmuseum"
-local version = "0.8.1"
+local version = "0.8.2"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://s3.amazonaws.com/chartmuseum/release/v" .. version .. "/bin/darwin/amd64/chartmuseum",
-            sha256 = "d5c30a6d00bea135e23a67c9193bd01608b850854deba803650c17e362a9c665",
+            sha256 = "c676eb42f81076049fb8305a4d827128ee4922badc217b713280e74df19c5b14",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://s3.amazonaws.com/chartmuseum/release/v" .. version .. "/bin/linux/amd64/chartmuseum",
-            sha256 = "bb26ca362143f0d95561c82cabf4af88fd8e3255f1bba56224a4cfe692290cb7",
+            sha256 = "5906ea624c6a8ef55f45de0c17e52cb724b1f099511939a1c2410375e5413858",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://s3.amazonaws.com/chartmuseum/release/v" .. version .. "/bin/windows/amd64/chartmuseum",
-            sha256 = "83baf7bc66f3335730d759a72ce14122c383fd189367b5ed79a1061a4b235181",
+            sha256 = "e8b08ddf22de2b6cbd1a42d2c086580ea7e82c04dd287dd138d7e41e9db6e3c9",
             resources = {
                 {
                     path = name,
