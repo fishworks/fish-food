4f354f92d0acf7d98fd96af4ef92a86513913a1b
Matthew Fisher <matt.fisher@microsoft.com>
2019-02-26 11:10:15 -0800

    serve 0.3.0
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- Food/serve.lua
+++ Food/serve.lua
@@ -1,5 +1,5 @@
 local name = "serve"
-local version = "0.2.0"
+local version = "0.3.0"
 
 food = {
     name = name,
@@ -11,7 +11,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/syntaqx/serve/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_macos_x86_64.tar.gz",
-            sha256 = "95951376c655c512578de440a2f455cfb1b3a8fc20dc34ac6e9474eba87846ba",
+            sha256 = "7f132af742033e3bf7b6c227a5f6a95f199ad292737634f4b2a75bce1e911f8d",
             resources = {
                 {
                     path = name,
@@ -24,7 +24,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/syntaqx/serve/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_linux_x86_64.tar.gz",
-            sha256 = "e66f59442252dcdc097e019aecb1426c71896a8135531271dd4fd0840a75e96c",
+            sha256 = "bba3ecc7ef861bbaee109fdca90c876247a2fdd41cf9a95a25bf44c70734a22b",
             resources = {
                 {
                     path = name,
@@ -37,7 +37,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/syntaqx/serve/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_windows_x86_64.zip",
-            sha256 = "d256f3b072227a969fb5ca93d3a946269e2d04e882ef2e033f0c4e0a4ab78baf",
+            sha256 = "9446e43f488c13261f3bb20136a5ee644c2cff1dba43881ca6784d82a921a2d8",
             resources = {
                 {
                     path = name .. ".exe",
