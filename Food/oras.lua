bcef596a1f92a991d630acdcf6de68bdb4297222
Josh Dolitsky <jdolitsky@gmail.com>
2019-04-19 13:24:44 -0500

    oras 0.4.0

--- Food/oras.lua
+++ Food/oras.lua
@@ -1,6 +1,6 @@
 local name = "oras"
 local org = "deislabs"
-local version = "0.3.3"
+local version = "0.4.0"
 
 food = {
     name = name,
@@ -13,7 +13,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/" .. org .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_linux_amd64.tar.gz",
-            sha256 = "821a31980c03cb6b5ee6e0e00d7af81a00d080bcdca7a754e37ed1a3453930fe",
+            sha256 = "5e33a68415040f50a38961840feba90db86a6f3a6ac9ed7d5d6979c4fd711a73",
             resources = {
                 {
                     path = name,
@@ -26,7 +26,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/" .. org .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_darwin_amd64.tar.gz",
-            sha256 = "0269fb4cffe68be5d7db063700579a1a2d2d02be85865ea63ac844025abdb6a9",
+            sha256 = "ce143447e0736011f5aaa5d5e653ddf2b0d82eed70728f999bf5bc5f52bc761c",
             resources = {
                 {
                     path = name,
@@ -39,7 +39,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/" .. org .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_windows_amd64.tar.gz",
-            sha256 = "7263a5a295ec64d765d378722d7349594773b7e47d71cddb773ece55fcca9bee",
+            sha256 = "9411f6f0ebd26acbc9c763d5aba88796486abaf7d626ae3474139d52c26340f8",
             resources = {
                 {
                     path = name .. ".exe",
