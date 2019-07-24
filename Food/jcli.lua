9f277ac5b7d1c893419c70a9cfcf7fee2b86cf8a
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-07-24 23:00:38 +0800

    fix the wrong path

--- Food/jcli.lua
+++ Food/jcli.lua
@@ -11,7 +11,7 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/linuxsuren/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. os .. arch .. ".tar.gz",
+            url = "https://github.com/linuxsuren/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-" .. os .. arch .. ".tar.gz",
             sha256 = "e78438f10c13ea5e836d7b4726efb1993ebe11fa9fc1589a46d9e48792dcb1d5",
             resources = {
                 {
@@ -24,7 +24,7 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/linuxsuren/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. os .. arch .. ".tar.gz",
+            url = "https://github.com/linuxsuren/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-" .. os .. arch .. ".tar.gz",
             sha256 = "6ae5095903b32695cd26d632d6082ef62545d23ddd247c4ab6aad2b4335c1d36",
             resources = {
                 {
@@ -37,7 +37,7 @@ food = {
         {
             os = "windows",
             arch = "386",
-            url = "https://github.com/linuxsuren/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. os .. "-" .. arch .. ".tar.gz",
+            url = "https://github.com/linuxsuren/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-" .. os .. "-" .. arch .. ".tar.gz",
             sha256 = "85d4bd2e206ae320376afb7f334c804e3792834a31c036e317a3ab25e56df4ca",
             resources = {
                 {
