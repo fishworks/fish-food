083ea348fb45c237c8e45c532d9401cafbd73326
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-07-27 21:58:08 +0800

    Change the org from linuxsuren to jenkins-zh

--- Food/jcli.lua
+++ Food/jcli.lua
@@ -11,7 +11,7 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/linuxsuren/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-darwin-amd64.tar.gz",
+            url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-darwin-amd64.tar.gz",
             sha256 = "e78438f10c13ea5e836d7b4726efb1993ebe11fa9fc1589a46d9e48792dcb1d5",
             resources = {
                 {
@@ -24,7 +24,7 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/linuxsuren/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-linux-amd64.tar.gz",
+            url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-linux-amd64.tar.gz",
             sha256 = "6ae5095903b32695cd26d632d6082ef62545d23ddd247c4ab6aad2b4335c1d36",
             resources = {
                 {
@@ -37,7 +37,7 @@ food = {
         {
             os = "windows",
             arch = "386",
-            url = "https://github.com/linuxsuren/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-window-386.tar.gz",
+            url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-window-386.tar.gz",
             sha256 = "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
             resources = {
                 {
