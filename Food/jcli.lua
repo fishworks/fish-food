e12dd02112f4482b232a9e51a1f9e32ea9281d81
Yanjun Shi <20575981+yJunS@users.noreply.github.com>
2019-08-01 23:05:33 +0800

    Update jcli.lua

--- Food/jcli.lua
+++ Food/jcli.lua
@@ -49,7 +49,7 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-window-386.tar.gz",
+            url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-windows-386.tar.gz",
             sha256 = "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
             resources = {
                 {
