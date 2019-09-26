4cc4dbd522bb27735b0dec5e47f46f3fbf623fb5
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-09-26 09:32:47 +0800

    Upgrade jcli to 0.0.20

--- Food/jcli.lua
+++ Food/jcli.lua
@@ -1,5 +1,5 @@
 local name = "jcli"
-local version = "0.0.19"
+local version = "0.0.20"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-darwin-amd64.tar.gz",
-            sha256 = "89ae6e193c3a84d1a5d763ab9fe149f9a72acd0344539d4e7231a338f171dc66",
+            sha256 = "644190a442500269e9c097f59244da1d40cfcd54af110d829aee78de360df902",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-linux-amd64.tar.gz",
-            sha256 = "83dbf29ee696a6600ec05be25e8a29f6afe215410ae72b737eebe142ed4a04f6",
+            sha256 = "9870c3c8fdcb9a808b03debd4992dbb607f52c20ba66cf81ec48b704baa6e5d2",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "386",
             url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-windows-386.tar.gz",
-            sha256 = "3f0c9bedcdfda00334e43c47f89c638e1cbd099a9983a51a12825a6fd81b685c",
+            sha256 = "55f493c6fd94f6aad57d8f93da6d8d4975d2291962f92e7a955b2d85c977161c",
             resources = {
                 {
                     path = name .. ".exe",
@@ -50,7 +50,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-windows-386.tar.gz",
-            sha256 = "3f0c9bedcdfda00334e43c47f89c638e1cbd099a9983a51a12825a6fd81b685c",
+            sha256 = "55f493c6fd94f6aad57d8f93da6d8d4975d2291962f92e7a955b2d85c977161c",
             resources = {
                 {
                     path = name .. ".exe",
