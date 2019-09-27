e166a38ea129073f84089e73c95e3fb48e5e470b
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-09-27 20:45:28 +0530

    go 1.13.1

--- Food/go.lua
+++ Food/go.lua
@@ -1,5 +1,5 @@
 local name = "go"
-local version = "1.13"
+local version = "1.13.1"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://dl.google.com/go/go" .. version .. ".darwin-amd64.tar.gz",
-            sha256 = "234ebbba1fbed8474340f79059cfb3af2a0f8b531c4ff0785346e0710e4003dd",
+            sha256 = "f3985fced3adecb62dd1e636cfa5eb9fea8f3e98101d9fcc4964d8f1ec255b7f",
             resources = {
                 {
                     path = "go/bin/go",
@@ -30,7 +30,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://dl.google.com/go/go" .. version .. ".linux-amd64.tar.gz",
-            sha256 = "68a2297eb099d1a76097905a2ce334e3155004ec08cdea85f24527be3c48e856",
+            sha256 = "94f874037b82ea5353f4061e543681a0e79657f787437974214629af8407d124",
             resources = {
                 {
                     path = "go/bin/go",
@@ -48,7 +48,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://dl.google.com/go/go" .. version .. ".windows-amd64.zip",
-            sha256 = "7d162b83157d3171961f8e05a55b7da8476244df3fac28a5da1c9e215acfea89",
+            sha256 = "24cb08d369c1962cccacedc56fd79dc130f623b3b667a316554621ad6ac9b442",
             resources = {
                 {
                     path = "go\\bin\\go.exe",
