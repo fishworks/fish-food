f1172f7018edb752d4734df96055071b05e8151d
Matt Boersma <Matt.Boersma@microsoft.com>
2019-10-11 09:29:33 -0600

    aks-engine v0.42.0

--- Food/aks-engine.lua
+++ Food/aks-engine.lua
@@ -1,5 +1,5 @@
 local name = "aks-engine"
-local version = "0.40.0"
+local version = "0.42.0"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "02559400f114d8ea84222d7a629b8491378d34191c81cffc4ef173b51522299a",
+            sha256 = "3544ce7d60177ac8c569502aad8b1a85d12a969480224a5f6e6219e1e636b1ab",
             resources = {
                 {
                     path = name .. "-v" .. version .. "-darwin-amd64/" .. name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "073d08a7d3061c2de5b638a1d516088acf58b13d2db2e1114c0b69f5d4cf41b5",
+            sha256 = "6e89253ef06254a6c51a0694f7db4e94498de1755b1f57196da6b9820a002ccf",
             resources = {
                 {
                     path = name .. "-v" .. version .. "-linux-amd64/" .. name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-windows-amd64.tar.gz",
-            sha256 = "af282eba336aa9cc44237836d2f3b5f63bb763a8a6860518dc171349b729b3f0",
+            sha256 = "09ae800cc9870e01a342f6a5be3fe92a8b875d870850bb9d2c4bb3cf0b738ade",
             resources = {
                 {
                     path = name .. "-v" .. version .. "-windows-amd64/" .. name .. ".exe",
