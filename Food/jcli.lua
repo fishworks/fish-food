41e12711c81d629ba039d53c3800204bd1473fcc
rick <rick@jenkins-zh.cn>
2019-10-31 21:17:32 +0800

    Release jcli v0.0.22

--- Food/jcli.lua
+++ Food/jcli.lua
@@ -1,5 +1,5 @@
 local name = "jcli"
-local version = "0.0.21"
+local version = "0.0.22"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-darwin-amd64.tar.gz",
-            sha256 = "6e30f65d8cb5d4c27061378f1ee4eeb682e08440203c4928b78350cd57d55cb4",
+            sha256 = "dcd330dcd7777d46d35af2637c8fa3dcdcd6ff8ca883af182818a7d514eafac0",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-linux-amd64.tar.gz",
-            sha256 = "c28cdb9192e46f91c73e758dd5e4b566c9b1ee8b42fce751f7cab407945186ae",
+            sha256 = "86db4df4b73c7b5106a964bfdcfe200f80c94c8b4ee982c80693880b4accf006",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "386",
             url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-windows-386.tar.gz",
-            sha256 = "edbee0fc1de82481dd3b661fa955e20753d1f531e24e69082e58883a436c464f",
+            sha256 = "3c128330c1a1c811c59ac5a1f2a26bc1027aef5db1c92e9b5b638de7c92c7861",
             resources = {
                 {
                     path = name .. ".exe",
@@ -50,7 +50,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v" .. version .. "/" .. name .. "-windows-386.tar.gz",
-            sha256 = "edbee0fc1de82481dd3b661fa955e20753d1f531e24e69082e58883a436c464f",
+            sha256 = "3c128330c1a1c811c59ac5a1f2a26bc1027aef5db1c92e9b5b638de7c92c7861",
             resources = {
                 {
                     path = name .. ".exe",
