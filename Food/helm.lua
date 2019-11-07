c1b4f8b96fc38e85a915420da2e5952630d4f1eb
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-11-07 21:23:25 +0530

    helm v2.16.0

--- Food/helm.lua
+++ Food/helm.lua
@@ -1,5 +1,5 @@
 local name = "helm"
-local version = "2.15.2"
+local version = "2.16.0"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://get.helm.sh/helm-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "0815dc44f9f1c5bcee1c130268bfca2ddf6425b60cf042d3b3c907c35ef11e31",
+            sha256 = "4544c03e044ad19424136fb66267f21b620eeee46dee9ec7735288081ca8d12a",
             resources = {
                 {
                     path = "darwin-amd64/" .. name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://get.helm.sh/helm-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "a9d2db920bd4b3d824729bbe1ff3fa57ad27760487581af6e5d3156d1b3c2511",
+            sha256 = "2d9124f582a33b269f48215a00922b3dcf7a03f265499886ba956fbc03ff9892",
             resources = {
                 {
                     path = "linux-amd64/" .. name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://get.helm.sh/helm-v" .. version .. "-windows-amd64.tar.gz",
-            sha256 = "372211639a9f489bd7976f6bbe4b5b7c3bcae486d1e29b267dc8ffa61621964e",
+            sha256 = "bf8dc26d4eb371702cb56eb65184865c3bb3ea52efac13b07becf6edf2df7654",
             resources = {
                 {
                     path = "windows-amd64\\" .. name .. ".exe",
