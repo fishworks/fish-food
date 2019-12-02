d0892cf9cb62045d402152474919cef80605375a
Matthew Fisher <matt.fisher@microsoft.com>
2019-12-02 10:49:52 -0800

    fix buffalo packages
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- Food/buffalo.lua
+++ Food/buffalo.lua
@@ -9,5 +9,43 @@ food = {
     homepage = "http://gobuffalo.io",
     version = version,
     packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://github.com/gobuffalo/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_Darwin_x86_64.tar.gz",
+            sha256 = "afd13b7a6ed44b5547a937c7b8ea4a3bd11f11673dfea726be1880078205eff6",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://github.com/gobuffalo/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_Linux_x86_64.tar.gz",
+            sha256 = "c367c71a6f01861eaafd825e058ed1249d418d4d0ff42a698264cbcad56f67bb",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://github.com/gobuffalo/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_Windows_x86_64.tar.gz",
+            sha256 = "18cec51a8f59f31f213cd44e3a787cd8bc25f39a1a9aac54ebaacf66d98a1fbd",
+            resources = {
+                {
+                    path = name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        }
     }
 }
