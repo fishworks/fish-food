962b787d070ff944b3631b1f5747e9fffd83682b
Matthew Fisher <matt.fisher@microsoft.com>
2019-12-03 08:11:23 -0800

    helm 2.16.1
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

---
+++ Food/helm@2.lua
@@ -0,0 +1,50 @@
+local name = "helm"
+local version = "2.16.1"
+
+food = {
+    name = name,
+    description = "The Kubernetes Package Manager",
+    license = "Apache-2.0",
+    homepage = "https://github.com/helm/helm",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://get.helm.sh/helm-v" .. version .. "-darwin-amd64.tar.gz",
+            sha256 = "34fc397ec4a992a451d130a962944315ca782242bbd05a8d732a2e74ca2b9153",
+            resources = {
+                {
+                    path = "darwin-amd64/" .. name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://get.helm.sh/helm-v" .. version .. "-linux-amd64.tar.gz",
+            sha256 = "7eebaaa2da4734242bbcdced62cc32ba8c7164a18792c8acdf16c77abffce202",
+            resources = {
+                {
+                    path = "linux-amd64/" .. name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://get.helm.sh/helm-v" .. version .. "-windows-amd64.tar.gz",
+            sha256 = "7ab28696e2f5a5b56001aacbe7156aae83c8fbd05946164efa3a2707aac33255",
+            resources = {
+                {
+                    path = "windows-amd64\\" .. name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        }
+    }
+}
