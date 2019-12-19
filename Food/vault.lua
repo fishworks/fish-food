02b179b4cb7e911251a0868888661b34fa846421
Frederik Mogensen <fmo@trifork.com>
2019-12-19 12:55:26 +0100

    vault 1.3.1

--- Food/vault.lua
+++ Food/vault.lua
@@ -1,5 +1,5 @@
 local name = "vault"
-local version = "1.3.0"
+local version = "1.3.1"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_darwin_amd64.zip",
-            sha256 = "6f6cbf11c7cdcd8fbbf2e9780b576a352fdc71bfee6299a6f8c71b9bfeb57b84",
+            sha256 = "23abadfcda33d5dffa238284c68061d6938aaaa11324301c042fe8d00b83fa7f",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_linux_amd64.zip",
-            sha256 = "d89b8a317831b06f2a32c56cb86071d058b09d9317b416bb509ce3d01e912eb3",
+            sha256 = "b49de8fd508eb9c2c222fa0f38e23546fb28991af2e8bfdb9bbe381a380f9baa",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_windows_amd64.zip",
-            sha256 = "153c608f5acfdf2a5b9f52c4b45aed4d194074a0598b2a2804a4ac75641516ab",
+            sha256 = "3703e3862653d38ba8dae0e8479f98924d2400c872922aaf3c3922988240464c",
             resources = {
                 {
                     path = name .. ".exe",
