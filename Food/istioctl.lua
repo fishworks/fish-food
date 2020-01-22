261eaaeeca050ac3db9b5379b0376c65aa89d93d
Frederik Mogensen <fmo@trifork.com>
2020-01-22 10:11:17 +0100

    istioctl 1.4.3

--- Food/istioctl.lua
+++ Food/istioctl.lua
@@ -1,7 +1,7 @@
 local name = "istioctl"
 local org = "istio"
-local release = "1.4.2"
-local version = "1.4.2"
+local release = "1.4.3"
+local version = "1.4.3"
 food = {
     name = name,
     description = "Connect, secure, control, and observe services.",
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/istio/istio" .. "/releases/download/" .. release .. "/istio" .. "-" .. version .. "-osx.tar.gz",
-            sha256 = "6f49a4d0148c483a501b726a7203686e98f8817a4b58d5ab498256e2424d05f6",
+            sha256 = "c555bb3eedc64f2185842038153c9e4701ac3f6e27aafd46659d160106969693",
             resources = {
                 {
                     path = "istio-" .. version .. "/bin/" .. name ,
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/istio/istio" .. "/releases/download/" .. release .. "/istio" .. "-" .. version .. "-linux.tar.gz",
-            sha256 = "8d2adfa1524a89d0263b7feadf6541b6427a8aecf8aa930f0e5eeb796661dd7b",
+            sha256 = "781e2c4391ba344d86fb6a4c321b76af31543e7479ea689dd7c7395fb1a41ff0",
             resources = {
                 {
                     path = "istio-" .. version .. "/bin/" .. name ,
@@ -39,7 +39,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/istio/istio" .. "/releases/download/" .. release .. "/istio" .. "-" .. version .. "-win.zip",
-            sha256 = "fa57910c7dd6d7f82ab87270ec26dde83cb0df5b16d4db5fae4118ca4a6249d7",
+            sha256 = "c7a2644736aa3a6b0d0d41c458891e81977d6c2847ed27050dc3964d20540e02",
             resources = {
                 {
                     path = "istio-" .. version .. "/bin/" .. name .. "exe",
