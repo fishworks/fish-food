2caefb4fb9da0af0d222b050ab0f860327773142
Frederik Mogensen <fmo@trifork.com>
2020-02-04 11:12:11 +0100

    oras 0.8.1

--- Food/oras.lua
+++ Food/oras.lua
@@ -1,7 +1,7 @@
 local name = "oras"
 local org = "deislabs"
-local release = "v0.8.0"
-local version = "0.8.0"
+local release = "v0.8.1"
+local version = "0.8.1"
 food = {
     name = name,
     description = "OCI Registry As Storage",
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/deislabs/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_darwin_amd64.tar.gz",
-            sha256 = "6f980791de4706584eb966ed51aa523526713810f3c918eae187be81f5ada83e",
+            sha256 = "22bb05b5d73ba5d8746dc6e52805cc212b9dafd84ebd8c14a88343701a4d645b",
             resources = {
                 {
                     path = name,
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/deislabs/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_linux_amd64.tar.gz",
-            sha256 = "690b019527f6478e91029750a0d111a87b6a67f68265d12ebcccd5d7832e35de",
+            sha256 = "da3eced65688163acc1a45c034a96a02fb54bc5eaee9d614340d0d7551a28ec7",
             resources = {
                 {
                     path = name,
@@ -39,7 +39,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/deislabs/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_windows_amd64.tar.gz",
-            sha256 = "abbdce29f6cec952ff77c24831814b74614fd49d044df1804de4ac36a4ec656d",
+            sha256 = "fc604d316bbbed2a31f4f8b8c94c276a46fa2a4e50ea12fb89667b2c47cbc2ce",
             resources = {
                 {
                     path = name .. ".exe",
