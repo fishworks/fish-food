eccd8fa3b5bbd1764e4b6466e61aeb372d7ec56f
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-03-06 17:44:28 +0530

    kubectl v1.17.3

--- Food/kubectl.lua
+++ Food/kubectl.lua
@@ -1,5 +1,5 @@
 local name = "kubectl"
-local version = "1.17.0"
+local version = "1.17.3"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://dl.k8s.io/v" .. version .. "/kubernetes-client-darwin-amd64.tar.gz",
-            sha256 = "fa92854143502e4aa90467511221f893d5b675953ad58acef6d231e2177fd300",
+            sha256 = "de28a617ad23aa11c06da2f47291e6a5043ac46d2400eef366ad56203ea0b255",
             resources = {
                 {
                     path = "kubernetes/client/bin/" .. name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://dl.k8s.io/v" .. version .. "/kubernetes-client-linux-amd64.tar.gz",
-            sha256 = "a5eb7e2e44d858d96410937a4e4c82f9087c9d120cb2b9e92462878eda59d578",
+            sha256 = "b4ec97998953723e108dcbb0ed3006501bbc28bf2b8262d284efff0d1aa1d154",
             resources = {
                 {
                     path = "kubernetes/client/bin/" .. name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://dl.k8s.io/v" .. version .. "/kubernetes-client-windows-amd64.tar.gz",
-            sha256 = "70ef84c26f4b560e35eed1a5145b555b42495e390cfb4537c6b5cb678e6a901f",
+            sha256 = "35d90f9ad8af528d9abd86b52e7955ddf680b2e21cd02b6a3504a8f2b4bba3f1",
             resources = {
                 {
                     path = "kubernetes\\client\\bin\\" .. name .. ".exe",
