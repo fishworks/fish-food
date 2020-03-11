19534d4712db9baa561acd2d8b99df117f5dad2d
Frederik Mogensen <fmo@trifork.com>
2020-03-11 10:17:13 +0100

    eksctl 0.14.0

--- Food/eksctl.lua
+++ Food/eksctl.lua
@@ -1,5 +1,5 @@
 local name = "eksctl"
-local version = "0.13.0"
+local version = "0.14.0"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/weaveworks/" .. name .. "/releases/download/" .. version .. "/" .. name .. "_Darwin_amd64.tar.gz",
             -- shasum of the release archive
-            sha256 = "b2aa450e2285d79e0631ba8410c709970585b2d34c7a7e7ee17ba38de221f01e",
+            sha256 = "6bb64d9514793eea4788fd1a219223466047e5d1f02c661e9a7bc46c661bd223",
             resources = {
                 {
                     path = name,
@@ -26,7 +26,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/weaveworks/" .. name .. "/releases/download/" .. version .. "/" .. name .. "_Linux_amd64.tar.gz",
             -- shasum of the release archive
-            sha256 = "73328372c14e8c0fb0571882a0497ee2e33d9c3d0e3d4987f035f07e4c4cd4a6",
+            sha256 = "7c670d460e7b23c55c589056b9ed034952233d65b293035cc46a706233620ce8",
             resources = {
                 {
                     path = name,
@@ -40,7 +40,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/weaveworks/" .. name .. "/releases/download/" .. version .. "/" .. name .. "_Windows_amd64.zip",
             -- shasum of the release archive
-            sha256 = "41b025473df428b934710c056a798566a2c2800a937ac2b9acd9a328dcb09d70",
+            sha256 = "3da0d3e5fbb173186b3ac8863fa0dcd2a3b96cd974050bdb305968f563c2e2be",
             resources = {
                 {
                     path = name .. ".exe",
