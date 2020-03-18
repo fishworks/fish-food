b5fc798b9a64f88821144f2e02c7d186b2609798
Matthew Fisher <matt.fisher@microsoft.com>
2020-03-18 09:52:29 -0700

    helm 3.1.2
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- Food/helm.lua
+++ Food/helm.lua
@@ -1,5 +1,5 @@
 local name = "helm"
-local version = "3.1.1"
+local version = "3.1.2"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://get.helm.sh/helm-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "2ce00e6c44ba18fbcbec21c493476e919128710d480789bb35bd228ae695cd66",
+            sha256 = "92b10652b05a150e76995e08910a662c200a8179cfdb16bd51766d0d5ecc981a",
             resources = {
                 {
                     path = "darwin-amd64/" .. name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://get.helm.sh/helm-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "cdd7ad304e2615c583dde0ffb0cb38fc1336cd7ce8ff3b5f237434dcadb28c98",
+            sha256 = "e6be589df85076108c33e12e60cfb85dcd82c5d756a6f6ebc8de0ee505c9fd4c",
             resources = {
                 {
                     path = "linux-amd64/" .. name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://get.helm.sh/helm-v" .. version .. "-windows-amd64.tar.gz",
-            sha256 = "c10aff979af0112ab9f181c2c40045eace48d6cc2587a330ae4f7960f91d05cd",
+            sha256 = "6c5550a979eaac35418686b56b39e25e68dedc759c963998fd17b972e73c2f1f",
             resources = {
                 {
                     path = "windows-amd64\\" .. name .. ".exe",
