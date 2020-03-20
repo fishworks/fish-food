76ad8f67b319063d12c055faf8fad20704acc3de
Masaki Muranaka <monaka@monami-ya.com>
2020-03-20 14:37:46 +0900

    argocd-1.4.2

---
+++ Food/argocd.lua
@@ -0,0 +1,37 @@
+local name = "argocd"
+local version = "1.4.2"
+food = {
+    name = name,
+    description = "Declarative continuous deployment for Kubernetes",
+    license = "Apache-2.0",
+    homepage = "https://argoproj.github.io/",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://github.com/argoproj/argo-cd/releases/download/v" .. version .. "/" .. name .. "-darwin-amd64",
+            sha256 = "671fd6859af9ae36fa62cd2a99b9437581b4d58102be5a355a68fd96a316b5a6",
+            resources = {
+                {
+                    path = name .. "-darwin-amd64",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://github.com/argoproj/argo-cd/releases/download/v" .. version .. "/" .. name .. "-linux-amd64",
+            sha256 = "25016ff4b51aa12b360d70c7d2b91d24bf17202ac534e7e06184e94a575152dd",
+            resources = {
+                {
+                    path = name .. "-linux-amd64",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        }
+    }
+}
