7fddba704aecf37e2888f9de85b8492032e9a437
Frederik Mogensen <fmo@trifork.com>
2020-03-26 08:45:47 +0100

    packer 1.5.5

--- Food/packer.lua
+++ Food/packer.lua
@@ -1,5 +1,5 @@
 local name = "packer"
-local version = "1.5.4"
+local version = "1.5.5"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_darwin_amd64.zip",
-            sha256 = "dab5ab9d4801da5206755856bc3f026942ce18391419202a1b0b442c1c2e591d",
+            sha256 = "899213557a79476406f8b6122725b0021d46337ee14e01e87f9f5f7cddce1d63",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_linux_amd64.zip",
-            sha256 = "c7277f64d217c7d9ccfd936373fe352ea935454837363293f8668f9e42d8d99d",
+            sha256 = "07f28a1a033f4bcd378a109ec1df6742ac604e7b122d0316d2cddb3c2f6c24d1",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_windows_amd64.zip",
-            sha256 = "26a66d6c99420b320babda2c94bfe1b289298b334d7721efd9f1a61139955d82",
+            sha256 = "9ed486093c2c4ea16cc57bdc80366105bd1ebb234f1ba1839243c552498766ff",
             resources = {
                 {
                     path = name .. ".exe",
