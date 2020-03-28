23848dcff41cfa2aa40c9ad0738e37ccec9399cd
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-03-28 15:55:47 +0530

    add nomad

---
+++ Food/nomad.lua
@@ -0,0 +1,49 @@
+local name = "nomad"
+local version = "0.10.5"
+
+food = {
+    name = name,
+    description = "Nomad is an easy-to-use, flexible, and performant workload orchestrator that can deploy a mix of microservice, batch, containerized, and non-containerized applications. Nomad is easy to operate and scale and has native Consul and Vault integrations.",
+    homepage = "https://nomadproject.io",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_darwin_amd64.zip",
+            sha256 = "3e56a43a9baec897d80a8811b1751e5e2c71618649fb1ce453ccac534c3dab06",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_linux_amd64.zip",
+            sha256 = "970eae241a1fec77002baccfa768bab4b7b4a352000b9615d4246498cd830838",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_windows_amd64.zip",
+            sha256 = "5e4127e481c9f80a39a02c116189629d9c428fcc521c39106f587c8d4163677f",
+            resources = {
+                {
+                    path = name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        }
+    }
+}
