d81e94fcb988bbb19972b2ae59f059584c5eff46
Frederik Mogensen <fmo@trifork.com>
2020-05-01 10:17:59 +0200

    kind 0.8.0

--- Food/kind.lua
+++ Food/kind.lua
@@ -1,7 +1,7 @@
 local name = "kind"
 local org = "kubernetes-sigs"
-local release = "v0.7.0"
-local version = "0.7.0"
+local release = "v0.8.0"
+local version = "0.8.0"
 food = {
     name = name,
     description = "Kubernetes IN Docker - local clusters for testing Kubernetes",
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/kubernetes-sigs/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-darwin-amd64",
-            sha256 = "11b8a7fda7c9d6230f0f28ffe57831a7227c0655dfb8d38e838e8f03db6612de",
+            sha256 = "d92729517f89a65398e52254ce9f3d6df4f33948e27a96006209f5a2d5559d23",
             resources = {
                 {
                     path = name .. "-darwin-amd64",
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/kubernetes-sigs/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-linux-amd64",
-            sha256 = "0e07d5a9d5b8bf410a1ad8a7c8c9c2ea2a4b19eda50f1c629f1afadb7c80fae7",
+            sha256 = "48b23b191225927bd0a2c5f7a6b4ea3610a0a0cbc6feca1099977d2368f90398",
             resources = {
                 {
                     path = name .. "-linux-amd64",
@@ -39,7 +39,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/kubernetes-sigs/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-windows-amd64",
-            sha256 = "253b7c330593f89ca904860c0da2b8b229f5515e41fd960e2b73a33d7e4c68e3",
+            sha256 = "5824f01be86df351ea5736f6f25c95acc3134654a7b6451ce43fa429a09c2feb",
             resources = {
                 {
                     path = name,
