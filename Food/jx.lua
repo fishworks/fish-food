5c3a95a72cf88fda0f3482dd85a702a5cdcd470d
John Greeley <jgreeley@bostonlighthouse.us>
2020-05-14 21:40:20 -0400

    adding jx recipe

---
+++ Food/jx.lua
@@ -0,0 +1,50 @@
+local name = "jx"
+local version = "2.1.35"
+
+food = {
+    name = name,
+    description = "JX is a command line tool for installing and using Jenkins X",
+    license = "Apache-2.0",
+    homepage = "https://jenkins-x.io/",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://github.com/jenkins-x/jx/releases/download/v" .. version .. "/" .. name .. "-darwin-amd64.tar.gz",
+            sha256 = "6dadf9e7215507baf9e154ff5c9cdfa994c5ac3598a7e33b1e212cee4e5f21c0",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://github.com/jenkins-x/jx/releases/download/v" .. version .. "/" .. name .. "-linux-amd64.tar.gz",
+            sha256 = "1d75d9d490a62852ea1f7c89f2980a029601ff45b07351cb62266f784eaddde1",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://github.com/jenkins-x/jx/releases/download/v" .. version .. "/" .. name .. "-windows-amd64.zip",
+            sha256 = "2df81b4348d1c3d0d248b25ec25d7ca359cea7d90108bb5f3fd845f1db839334",
+            resources = {
+                {
+                    path = name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        }
+    }
+}
