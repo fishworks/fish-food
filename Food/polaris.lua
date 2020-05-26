31f31267ebe196c2fe9638aa92f247c76ed89f86
Frederik Mogensen <fmo@trifork.com>
2020-05-26 13:10:51 +0200

    polaris 1.0.2

--- Food/polaris.lua
+++ Food/polaris.lua
@@ -1,7 +1,7 @@
 local name = "polaris"
 local org = "FairwindsOps"
-local release = "0.6.0"
-local version = "0.6.0"
+local release = "1.0.2"
+local version = "1.0.2"
 food = {
     name = name,
     description = "Validation of best practices in your Kubernetes clusters",
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/FairwindsOps/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_Darwin_x86_64.tar.gz",
-            sha256 = "cd153b3d0eee70ebb73f898f6d39b1e3689669d2db4e1f79b705e1575c5ecf12",
+            sha256 = "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
             resources = {
                 {
                     path = name,
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/FairwindsOps/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_Linux_x86_64.tar.gz",
-            sha256 = "c509b2cd8286bb37dadf3433c74ee0c29559b264ae3f54b14a25fa522fff2c42",
+            sha256 = "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
             resources = {
                 {
                     path = name,
