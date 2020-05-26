c76b93d2628a46c8e9b45bc345659bc05769f7f3
Frederik Mogensen <fmo@trifork.com>
2020-05-26 12:46:11 +0200

    k9s 0.19.7

--- Food/k9s.lua
+++ Food/k9s.lua
@@ -1,7 +1,7 @@
 local name = "k9s"
 local org = "derailed"
-local release = "v0.19.6"
-local version = "0.19.6"
+local release = "v0.19.7"
+local version = "0.19.7"
 food = {
     name = name,
     description = "🐶 Kubernetes CLI To Manage Your Clusters In Style!",
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/derailed/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_Darwin_x86_64.tar.gz",
-            sha256 = "f7ea78c843c13ae6069c8d61b468bdd81f4cb9471623d1909109479c60703443",
+            sha256 = "970e6d483af6713eb533b682926237197d14c91cb9245dd35af76d028fd8cad6",
             resources = {
                 {
                     path = name,
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/derailed/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_Linux_x86_64.tar.gz",
-            sha256 = "7fbe0cbf22f3a86ca5b9118e3324702550e5bfde2e080fff4d589da611e26d0e",
+            sha256 = "d550594356f34bbcdbe2327ae2ac4144d0ce8f720707cf3228e57fe9df0d8d4a",
             resources = {
                 {
                     path = name,
@@ -39,7 +39,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/derailed/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_Windows_x86_64.tar.gz",
-            sha256 = "8d723947f5979f9c0c5d4c2dba44be1ce03ed8f1ead30e7ffe41d370e2eaf885",
+            sha256 = "2d399d7fd4291bc9e16030743bf81f0a78fb7719544712b8f4645f41d99dcc0c",
             resources = {
                 {
                     path = name .. ".exe",
