c757b8893b201d8de6e385e68a8575ed385f594a
Matthew Fisher <matt.fisher@microsoft.com>
2020-06-09 15:40:48 -0700

    fix letitgo
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- Food/letitgo.lua
+++ Food/letitgo.lua
@@ -11,8 +11,8 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/NoUseFreak/letitgo/releases/download/0.8.3/darwin_amd64.zip",
-            sha256 = "4df069b249a945086028b204b6dad495cf71de54d79f3688f2d8b9b871610dfe",
+            url = "https://github.com/NoUseFreak/letitgo/releases/download/" .. version .. "/darwin_amd64.zip",
+            sha256 = "38f6390f81f91f21d71a7a01e4b3bfefc06b9e13413439ea7aa64f944ca4661c",
             resources = {
                 {
                     path = name,
@@ -24,8 +24,8 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/NoUseFreak/letitgo/releases/download/0.8.3/linux_amd64.zip",
-            sha256 = "b72c680f1e278bb1b581cbf884ed1ebe78ea8b6a14d528feeeb7cc79af3927b7",
+            url = "https://github.com/NoUseFreak/letitgo/releases/download/" .. version .. "/linux_amd64.zip",
+            sha256 = "1aaf9b2d123bb28215af5508257b6eba7b5c115c3debba36597341749a2b9562",
             resources = {
                 {
                     path = name,
@@ -37,8 +37,8 @@ food = {
         {
             os = "linux",
             arch = "i386",
-            url = "https://github.com/NoUseFreak/letitgo/releases/download/0.8.3/linux_386.zip",
-            sha256 = "7199fb7ce8ed5e3239b488f0bf79f7be855921b01eea8f0b8fbaf0f0480c911a",
+            url = "https://github.com/NoUseFreak/letitgo/releases/download/" .. version .. "/linux_386.zip",
+            sha256 = "b56c6ea895aab972048f9329fe465edb0e9852d0c983df35cce42e4b93f97691",
             resources = {
                 {
                     path = name,
@@ -50,8 +50,8 @@ food = {
         {
             os = "linux",
             arch = "arm64",
-            url = "https://github.com/NoUseFreak/letitgo/releases/download/0.8.3/linux_arm.zip",
-            sha256 = "65bfc8f3fedae7373fe59e24e1e24e5aa6c722b5544d67e8dd06b768824b8b08",
+            url = "https://github.com/NoUseFreak/letitgo/releases/download/" .. version .. "/linux_arm.zip",
+            sha256 = "0745dd05d3607a6bf95b38d336d33e60fadc9aba5074ca8ba1fcf807ea6f98d3",
             resources = {
                 {
                     path = name,
@@ -62,4 +62,3 @@ food = {
         }
     }
 }
-
