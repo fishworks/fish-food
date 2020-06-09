fa344f3cea828710c45dad4f39e179ac123aee5b
Matthew Fisher <matt.fisher@microsoft.com>
2020-06-09 15:36:27 -0700

    wasm-to-oci 0.1.1
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

---
+++ Food/wasm-to-oci.lua
@@ -0,0 +1,51 @@
+local name = "wasm-to-oci"
+local version = "0.1.1"
+local githubReleaseDownloadURL = "https://github.com/engineerd/wasm-to-oci/releases/download"
+
+food = {
+    name = name,
+    description = "Use OCI registries to distribute WASM modules",
+    license = "MIT",
+    homepage = "https://github.com/engineerd/wasm-to-oci",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = githubReleaseDownloadURL .. "/v" .. version .. "/" .. "darwin-amd64-" .. name,
+            sha256 = "3a612230dc9f4dafbf6fadbe0f871ad60c3d5d74f687c35acb388701814214a3",
+            resources = {
+                {
+                    path = "darwin-amd64-" .. name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = githubReleaseDownloadURL .. "/v" .. version .. "/" .. "linux-amd64-" .. name,
+            sha256 = "54eb3091554cd79a9c7db355e509ac37a1b75e9b743adc2c7f8563ef14606d3f",
+            resources = {
+                {
+                    path = "linux-amd64-" .. name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = githubReleaseDownloadURL .. "/v" .. version .. "/" .. "windows-amd64-" .. name .. ".exe",
+            sha256 = "0ac66370cd348887aa34708eb4a7fe7fdeeef44a9f5401f971ddf95485bc98d1",
+            resources = {
+                {
+                    path = "windows-amd64-" .. name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        }
+    }
+}
