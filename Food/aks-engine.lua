546bb273f6ae84d9b1dcb0758a8b6be07d3b63e9
Frederik Mogensen <fmo@trifork.com>
2020-06-30 08:02:40 +0200

    aks-engine 0.53.0

--- Food/aks-engine.lua
+++ Food/aks-engine.lua
@@ -1,5 +1,5 @@
 local name = "aks-engine"
-local version = "0.52.0"
+local version = "0.53.0"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "a1ea61f0184066b17ee77b038d41de2d751ae7e18240c5221dd2fdec4fdd7695",
+            sha256 = "9753ae612760d1572fb77e12c5c365202aa18d98a1c8c6dd433b911f4a3fa320",
             resources = {
                 {
                     path = name .. "-v" .. version .. "-darwin-amd64/" .. name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "bd168e3e2132010ae3df0089c2a4b223680a7a5269466605b9e0849fdf56449a",
+            sha256 = "bb4fcaa2884cae88ed715dcceab3f2f6c4f93f8ba723d2e898388e72cb6b62a6",
             resources = {
                 {
                     path = name .. "-v" .. version .. "-linux-amd64/" .. name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-windows-amd64.tar.gz",
-            sha256 = "649455e35275be359d63a8f5b78dc99ed2a5d1cecbecca076784c1349462e079",
+            sha256 = "97f728d14fae32b2fd07bcc42cd3c515424ce5f41d429a09e1d69b16669cfac7",
             resources = {
                 {
                     path = name .. "-v" .. version .. "-windows-amd64/" .. name .. ".exe",
