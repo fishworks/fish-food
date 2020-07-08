20230b1c80d8589d49a11cd2a0cc5c1d9508f329
Matthew Fisher <matt.fisher@microsoft.com>
2020-07-08 12:53:22 -0700

    k3d 1.7.0
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

---
+++ Food/k3d.lua
@@ -0,0 +1,91 @@
+local name = "k3d"
+local version = "1.7.0"
+local release = "v" .. version
+
+food = {
+    name = name,
+    description = "k3s in docker",
+    license = "MIT",
+    homepage = "https://k3d.io/",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            
+            url = "https://github.com/rancher/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-darwin-amd64",
+            sha256 = "c24add17c29c95c239efba277977cb1ecdfb77255d57ade004059cb04bf28250",
+            resources = {
+                {
+                    path = name .. "-darwin-amd64",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://github.com/rancher/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-linux-amd64",
+            sha256 = "da9ff31bcf4377fadfb065f4998d347f19de1168a5a553ce2c23b763ee1f6098",
+            resources = {
+                {
+                    path = name .. "-linux-amd64",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "386",
+            url = "https://github.com/rancher/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-linux-386",
+            sha256 = "d42d3128ab9f0586187b4fe977b1560b86b7fe787f0d0de40e70c971b90e17b4",
+            resources = {
+                {
+                    path = name .. "-linux-386",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "arm",
+            url = "https://github.com/rancher/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-linux-arm",
+            sha256 = "d922dc3c6df0cb9c7a92a3d1f1a96efd4126208596b06f522d6270289dcfadd5",
+            resources = {
+                {
+                    path = name .. "-linux-arm",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "arm64",
+            url = "https://github.com/rancher/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-linux-arm64",
+            sha256 = "2d08ee5434fa6a9f6d8abf420cb3ebb075af320262ecbac82175e33442163587",
+            resources = {
+                {
+                    path = name .. "-linux-arm64",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://github.com/rancher/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-windows-amd64.exe",
+            sha256 = "1f8e8515f281a8439bea5605c122ac4f45192c3f043f315f22250bd23f36e661",
+            resources = {
+                {
+                    path = name .. "-windows-amd64.exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        }
+    }
+}
