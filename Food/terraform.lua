134705b4a21543bce382e194a79af70bfd329f07
Frederik Mogensen <fmo@trifork.com>
2020-08-27 08:17:24 +0200

    terraform 0.13.1

--- Food/terraform.lua
+++ Food/terraform.lua
@@ -1,5 +1,5 @@
 local name = "terraform"
-local version = "0.13.0"
+local version = "0.13.1"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_darwin_amd64.zip",
-            sha256 = "080af0420732cd08941aa4c0d2b4693056b24366724faa11b107bf052f7de203",
+            sha256 = "fe5d1b6e22892c5dcc8b44d2a26ea1e29d90af6fcb1472f3881ca3c08c8a8084",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_linux_amd64.zip",
-            sha256 = "9ed437560faf084c18716e289ea712c784a514bdd7f2796549c735d439dbe378",
+            sha256 = "f7b842d1c06045b496fd00db83520e83a974a294b070dbaf88cb1013c5f02caf",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://releases.hashicorp.com/" .. name .. "/" .. version .. "/" .. name .. "_" .. version .. "_windows_amd64.zip",
-            sha256 = "8af85914d8804c521152167749ca680d7d51447127deb2c7853835b6c62aa9ed",
+            sha256 = "fa7b17147baf4aca478ae83b25c14c1baa064a118a21e0e7b34e3d70e1732b6d",
             resources = {
                 {
                     path = name .. ".exe",
