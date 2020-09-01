8855e103398f1989d0d2fa695d9b60eca9f4ab51
Frederik Mogensen <fmo@trifork.com>
2020-09-01 10:19:22 +0200

    gomplate 3.8.0

--- Food/gomplate.lua
+++ Food/gomplate.lua
@@ -1,9 +1,9 @@
 local name = "gomplate"
-local version = "3.7.0"
-
+local release = "v3.8.0"
+local version = "3.8.0"
 food = {
     name = name,
-    description = "A versatile Go template processor",
+    description = "A flexible commandline tool for template rendering. Supports lots of local and remote datasources.",
     license = "MIT",
     homepage = "https://gomplate.ca",
     version = version,
@@ -11,9 +11,8 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/hairyhenderson/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_darwin-amd64-slim",
-            -- shasum of the release archive
-            sha256 = "e574ccecbae40b90ea6ab590deb1c1e7697961d0782638b0104a73947cd529b3",
+            url = "https://github.com/hairyhenderson/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_darwin-amd64-slim",
+            sha256 = "60217c40bdf65c781a0f5933fe5cba04d93005bf15e2e479e694895481d11b1b",
             resources = {
                 {
                     path = name .. "_darwin-amd64-slim",
@@ -25,9 +24,8 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/hairyhenderson/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_linux-amd64-slim",
-            -- shasum of the release archive
-            sha256 = "edeafbade658f4cf8dde853b7b2d8b8441936587bded5d32444efee3d3be9b87",
+            url = "https://github.com/hairyhenderson/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_linux-amd64-slim",
+            sha256 = "847f7d9fc0dc74c33188c2b0d0e9e4ed9204f67c36da5aacbab324f8bfbf29c9",
             resources = {
                 {
                     path = name .. "_linux-amd64-slim",
@@ -39,13 +37,12 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/hairyhenderson/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_windows-amd64-slim" .. ".exe",
-            -- shasum of the release archive
-            sha256 = "505e98480cd9355c570780496b0e93b18dbe5020db2e4ce07125168b1b6e985e",
+            url = "https://github.com/hairyhenderson/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_windows-amd64-slim.exe",
+            sha256 = "77e907bddd6c6474ca1137b51a22eb1ec72d8a113bc3aa92e4825b993399069c",
             resources = {
                 {
-                    path = name .. "_windows-amd64-slim" .. ".exe",
-                    installpath = "bin\\" .. name .. ".exe",
+                    path = name .. "_windows-amd64-slim.exe",
+                    installpath = "bin\\" .. name .. ".exe"
                 }
             }
         }
