1ad9f414908f58f964a420fc173f6aa5855794e1
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-09-09 09:27:27 +0200

    Update aks-engine.lua

--- Food/aks-engine.lua
+++ Food/aks-engine.lua
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "359d905cdd795e650e2d1adf7e0176dabebfc6062dfa9fb2bda5236889ebc5c8",
+            sha256 = "aed06100144bb4e7361ff9312222bf52ba71a8eec2bb8c90156e33a0eb59ec35",
             resources = {
                 {
                     path = name .. "-v" .. version .. "-darwin-amd64/" .. name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "644f98abf0738e3ad27c45cb37a9b0c96e879b7725fc08a23e51de99d303c467",
+            sha256 = "00438d6928d7b97da86e028ba1b3e7ce054ec18f3938e8f53f9fe9e7fdc0c0fa",
             resources = {
                 {
                     path = name .. "-v" .. version .. "-linux-amd64/" .. name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-windows-amd64.tar.gz",
-            sha256 = "ece37607d826d11efa2414dcaa56d97207857bc1d297acef1b44bffdcc8c146a",
+            sha256 = "6a560f274045c089699ba5c4b245abb9edd3b93ed3ca4ae1a4158bfd848b20b4",
             resources = {
                 {
                     path = name .. "-v" .. version .. "-windows-amd64/" .. name .. ".exe",
