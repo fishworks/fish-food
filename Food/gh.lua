bee244f2722091ecf4b2403cff43dbecdc844f1a
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-09-16 07:44:03 +0200

    added github cli

---
+++ Food/gh.lua
@@ -0,0 +1,53 @@
+local name = "gh"
+local version = "0.12.0"
+
+food = {
+    name = name,
+    description = "GitHub’s official command line tool",
+    homepage = "https://github.com/cli/cli",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://github.com/cli/cli/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_macOS_amd64.tar.gz",
+            -- shasum of the release archive
+            sha256 = "f5ee17cbfe23694ab36fdb9f1d31a5cbdd80f33606842c75262e8e3f9047e9ef",
+            resources = {
+                {
+                    path = name .. "_" .. version .. "_linux_amd64" .. "/bin/" .. name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://github.com/cli/cli/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_linux_amd64.tar.gz",
+            -- shasum of the release archive
+            sha256 = "bd289cd43703d56038e8c95eb1e937659be128873ee3669c9b8a92f61be2cebd",
+            resources = {
+                {
+                    path = name .. "_" .. version .. "_linux_amd64" .. "/bin/" .. name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://github.com/cli/cli/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_windows_amd64.zip",
+            -- shasum of the release archive
+            sha256 = "6f79296f7c37121ee5853d79a1241eb7d3ba892d491953db1d375a498c4b84ad",
+            resources = {
+                {
+                    path = "bin/" .. name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        }
+    }
+}
+
