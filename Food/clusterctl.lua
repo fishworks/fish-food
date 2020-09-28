cc07f854f3e3364a9d6f9130aa559c92a1f022ce
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-09-28 11:37:19 +0200

    added cluster-api clusterctl

---
+++ Food/clusterctl.lua
@@ -0,0 +1,43 @@
+local name = "clusterctl"
+local version = "0.3.9"
+local release = "v" .. version
+local org = "kubernetes-sigs"
+local repo = "cluster-api"
+local url = "https://github.com/" .. org .. "/" .. repo
+
+food = {
+    name = name,
+    description = "Cluster API command line tool",
+    homepage = "https://github.com/kubernetes-sigs/cluster-api",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = url .. "/releases/download/" .. release .. "/" .. name .. "-darwin-amd64",
+            -- shasum of the release archive
+            sha256 = "5d5accbafa85da99e2f3d45188a4bce4531cdeacbf5bba04f1352c9dcbad9eac",
+            resources = {
+                {
+                    path = name .. "-darwin-amd64",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = url .. "/releases/download/" .. release .. "/" .. name .. "-linux-amd64",
+            -- shasum of the release archive
+            sha256 = "1b460f28b58d796b11d1e51634d2593e2823deac3e6554f9a9a8678a4ba4032e",
+            resources = {
+                {
+                    path = name .. "-linux-amd64",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        }
+    }
+}
