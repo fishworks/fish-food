a9c39edc3990278628155b2b95399fca8871f657
Frederik Mogensen <fmo@trifork.com>
2020-10-01 10:14:27 +0200

    tilt 0.17.6

---
+++ Food/tilt.lua
@@ -0,0 +1,50 @@
+local name = "tilt"
+local release = "v0.17.6"
+local version = "0.17.6"
+food = {
+    name = name,
+    description = "A multi-service dev environment for teams on Kubernetes",
+    license = "Apache-2.0",
+    homepage = "https://tilt.dev/",
+    version = version,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://github.com/tilt-dev/" .. name .. "/releases/download/" .. release .. "/" .. name .. "." .. version .. ".mac.x86_64.tar.gz",
+            sha256 = "142fb65d1b53a3bff228f8b95ca6def2661594973d8c1fc490dc18f6e1a4b427",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://github.com/tilt-dev/" .. name .. "/releases/download/" .. release .. "/" .. name .. "." .. version .. ".linux.x86_64.tar.gz",
+            sha256 = "ea06b7e19e3586b5bad9fc689eec9b64c45a45fe546f61c4c83607cd21562a25",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://github.com/tilt-dev/" .. name .. "/releases/download/" .. release .. "/" .. name .. "." .. version .. ".windows.x86_64.zip",
+            sha256 = "df3121b01d3d8e44c010e8fc41ccc9102e9e36527dbbb3a4000c4a305dcc181c",
+            resources = {
+                {
+                    path = name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        }
+    }
+}
