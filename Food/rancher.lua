ac3aeea33f56626e651c0124e42f902ac34b9116
Frederik Mogensen <fmotrifork>
2020-11-02 08:41:43 +0100

    rancher 2.4.7

--- Food/rancher.lua
+++ Food/rancher.lua
@@ -1,5 +1,5 @@
 local name = "rancher"
-local version = "2.4.6"
+local version = "2.4.7"
 local release = "v" .. version
 
 food = {
@@ -13,7 +13,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/rancher/cli/releases/download/" .. release .. "/" .. name .. "-darwin-amd64-" .. release .. ".tar.gz",
             -- shasum of the release archive
-            sha256 = "4616fcb64bc0e5bd63cd68d432990ba58acbc69233362ee75375fa9cc9729287",
+            sha256 = "3ef0b82635a06ee5c2d39ff0a5b23061eb239c78e55fadff59456d2cc412ded9",
             resources = {
                 {
                     path = name .. "-" .. release .. "/",
@@ -27,7 +27,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/rancher/cli/releases/download/" .. release .. "/" .. name .. "-linux-amd64-" .. release .. ".tar.gz",
             -- shasum of the release archive
-            sha256 = "e4221b2055bd3488ac5b7543e20bf37c16b09ca21026170d746163b740291ef9",
+            sha256 = "b123c3bdf7e10267cef4b14147e7c6cf83479201f5e601515ff7d54c676fc0e9",
             resources = {
                 {
                     path = name .. "-" .. release .. "/" .. name,
@@ -41,7 +41,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/rancher/cli/releases/download/" .. release .. "/" .. name .. "-windows-amd64-" .. release .. ".zip",
             -- shasum of the release archive
-            sha256 = "c6653495cfdb5d4aaa5754a9d14d4294c3c2f18bd9f095e965f0480b65a2ada2",
+            sha256 = "022105a37f5cf26e4939b52bfec7d4ff8d70a01d8e525a9bece9d5811f013730",
             resources = {
                 {
                     path = name .. "-" .. release .. "/" .. name .. ".exe",
