fa0a2a0f81fa6024d2cff075dc75f7c53ee7d2db
Frederik Mogensen <fmotrifork>
2020-12-18 09:11:17 +0100

    polaris 3.0.0

--- Food/polaris.lua
+++ Food/polaris.lua
@@ -1,5 +1,5 @@
 local name = "polaris"
-local version = "1.2.1"
+local version = "3.0.0"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/FairwindsOps/" .. name .. "/releases/download/" .. version .. "/" .. name .. "_" .. version .. "_darwin_amd64.tar.gz",
-            sha256 = "9252316942503097c6f4c053e4fb90ae5a918a4c9a9ac6781122e896a692a1cd",
+            sha256 = "a7924df6fe4e14c2e4945bfbd37d7d4a0350b61abedf4143a2b1d1713aca061d",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/FairwindsOps/" .. name .. "/releases/download/" .. version .. "/" .. name .. "_" .. version .. "_linux_amd64.tar.gz",
-            sha256 = "76f1aabde7d21740e4d128c8ba944e55638fd3e17c12dbe5b552053c40ebc62f",
+            sha256 = "43651c29e0d5036eb6dff00cf35a166204d69e30ff8ac1350ca2956a854430c1",
             resources = {
                 {
                     path = name,
