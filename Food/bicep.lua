cda745e1080deba556fda9f2b59f092bf8fd2506
Frederik Mogensen <fmotrifork>
2021-01-04 10:54:05 +0100

    bicep 0.2.212

--- Food/bicep.lua
+++ Food/bicep.lua
@@ -1,5 +1,5 @@
 local name = "bicep"
-local version = "0.2.59"
+local version = "0.2.212"
 local release = "v" .. version
 
 
@@ -14,7 +14,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-osx-x64",
             -- shasum of the release archive
-            sha256 = "9b07831f1ac59a613ed8f95f23e691387ad2f3f0d70872b0b4737c4736c09013",
+            sha256 = "fe47919059bf1334c1e3453e4f760fe1b41ca04b99d219acd3c34f4cca522dd7",
             resources = {
                 {
                     path = name .. "-osx-x64",
@@ -28,7 +28,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-linux-x64",
             -- shasum of the release archive
-            sha256 = "13eefdbe105b8a162b3bdebf247fc6ec3fb7cf838c917d04775249531ba70873",
+            sha256 = "ba1ba9a199f0f1c12c24757c14f80f65a755f822115e0e471a61801bd4a02593",
             resources = {
                 {
                     path = name .. "-linux-x64",
@@ -42,7 +42,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/Azure/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-win-x64.exe",
             -- shasum of the release archive
-            sha256 = "150f1cb9be8c9fd0b15fce653f1def1920b88462fa76ee7e183a755d5857a533",
+            sha256 = "c42d99dfe9123298aef15271320974520548a0fda5abaf15f21fa1337eb6bbfc",
             resources = {
                 {
                     path = name .. "-win-x64.exe",
