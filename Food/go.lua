712e2a9fbe323feab363f246dcae2dfedb19d766
Sergey Novikov <novikov.su@gmail.com>
2021-01-26 15:20:37 +0500

    go 1.15.7

--- Food/go.lua
+++ Food/go.lua
@@ -1,5 +1,5 @@
 local name = "go"
-local version = "1.15.6"
+local version = "1.15.7"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://dl.google.com/go/go" .. version .. ".darwin-amd64.tar.gz",
-            sha256 = "940a73b45993a3bae5792cf324140dded34af97c548af4864d22fd6d49f3bd9f",
+            sha256 = "af423736fffded2b588bab13b8963ad071eb47600ec83d0304a9a3ab95ef49a0",
             resources = {
                 {
                     path = "go/bin/go",
@@ -30,7 +30,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://dl.google.com/go/go" .. version .. ".linux-amd64.tar.gz",
-            sha256 = "3918e6cc85e7eaaa6f859f1bdbaac772e7a825b0eb423c63d3ae68b21f84b844",
+            sha256 = "0d142143794721bb63ce6c8a6180c4062bcf8ef4715e7d6d6609f3a8282629b3",
             resources = {
                 {
                     path = "go/bin/go",
@@ -48,7 +48,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://dl.google.com/go/go" .. version .. ".windows-amd64.zip",
-            sha256 = "b7b3808bb072c2bab73175009187fd5a7f20ffe0a31739937003a14c5c4d9006",
+            sha256 = "6e8f680118d9dfbd7ee61ed2b3d319f278d41de5757b6e30ed190fa9c3ee5767",
             resources = {
                 {
                     path = "go\\bin\\go.exe",
