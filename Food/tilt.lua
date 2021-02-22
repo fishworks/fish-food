39baa15783c3ced14c51e69df27968206e48a68c
Frederik Mogensen <fmotrifork>
2021-02-22 08:35:41 +0100

    tilt 0.18.10

--- Food/tilt.lua
+++ Food/tilt.lua
@@ -1,5 +1,5 @@
 local name = "tilt"
-local version = "0.18.9"
+local version = "0.18.10"
 local release = "v" .. version
 
 food = {
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/tilt-dev/" .. name .. "/releases/download/" .. release .. "/" .. name .. "." .. version .. ".mac.x86_64.tar.gz",
-            sha256 = "0640a0a5132a863f702f8cced8b95219c0e9d1533becbf6a2370b970740e99de",
+            sha256 = "cd4e654e8f69453bd163fcb9c69cc130eabd3f064cea7749ed6c1f37e089c396",
             resources = {
                 {
                     path = name,
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/tilt-dev/" .. name .. "/releases/download/" .. release .. "/" .. name .. "." .. version .. ".linux.x86_64.tar.gz",
-            sha256 = "130b49a7600975fbb2a7513a8e27d3790c2b0c42a1396761b18c4cf4bdb0ed6b",
+            sha256 = "c208cf329dbb0352f9ef0f27866ac3fded70d3551e8587a99afdd5160e4424a3",
             resources = {
                 {
                     path = name,
@@ -39,7 +39,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/tilt-dev/" .. name .. "/releases/download/" .. release .. "/" .. name .. "." .. version .. ".windows.x86_64.zip",
-            sha256 = "827114de23b4bd3e08ab79a16c5ee5b85e879b6a1f3b7a21735f636fb7bf0024",
+            sha256 = "05bbe17223ff9641c1588431e2c7e0be6e34a210c3ca4085cbe93ba5e353126c",
             resources = {
                 {
                     path = name .. ".exe",
