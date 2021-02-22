9e5a5162767de6320eff252489def85fb0a70007
Frederik Mogensen <fmotrifork>
2021-02-22 08:34:38 +0100

    hugo 0.81.0

--- Food/hugo.lua
+++ Food/hugo.lua
@@ -1,5 +1,5 @@
 local name = "hugo"
-local version = "0.80.0"
+local version = "0.81.0"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/gohugoio/" .. name .. "/releases/download/v" .. version .. "/" .. "hugo_" .. version .. "_macOS-64bit.tar.gz",
-            sha256 = "b5b19e22a2e741b63289e60cafb8035c55d1d6cafebaa8a7ac440e5f4ecb80f0",
+            sha256 = "216e89231a631a76be4866d6a51af8e3757107dfa963efe6976a2ef843d181de",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/gohugoio/" .. name .. "/releases/download/v" .. version .. "/" .. "hugo_" .. version .. "_Linux-64bit.tar.gz",
-            sha256 = "b3a259bbe633e2f9182f8ecfc1b5cee6a7cfc4c970defe5f29c9959f2ef3259b",
+            sha256 = "35099b33780075ed9c709296f371d6b9fd44fa9ed3d26af4266ed6be4097250d",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/gohugoio/" .. name .. "/releases/download/v" .. version .. "/" .. "hugo_" .. version .. "_Windows-64bit.zip",
-            sha256 = "619e55b56e36d42afee3985779541d129b285a4f3183ba569012e6a417c53555",
+            sha256 = "8e0e8a820f308711f48e3ab56a42044fa461295e7f245cc4b500af436131988f",
             resources = {
                 {
                     path = name .. ".exe",
