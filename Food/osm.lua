2c9f43040fc02509322b50e092d7d602442c09de
Frederik Mogensen <fmotrifork>
2021-02-23 08:53:57 +0100

    osm 0.7.0

--- Food/osm.lua
+++ Food/osm.lua
@@ -1,21 +1,34 @@
 local name = "osm"
-local version = "0.3.0"
-
+local release = "v0.7.0"
+local version = "0.7.0"
 food = {
     name = name,
     description = "Open Service Mesh (OSM) is a lightweight, extensible, cloud native service mesh that allows users to uniformly manage, secure, and get out-of-the-box observability features for highly dynamic microservice environments.",
+    license = "Apache-2.0",
     homepage = "https://openservicemesh.io/",
     version = version,
     packages = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/openservicemesh/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
-            -- shasum of the release archive
-            sha256 = "454b3be1c65ca2a26a1a6ae4fcfaa541fd8a89c15f9459a1acf5bc679fe60f15",
+            url = "https://github.com/openservicemesh/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
+            sha256 = "7560a693aa079ca5c5a3db8b8e49293e7a6d9eea2c7f4e99b50b3be9802610ee",
             resources = {
                 {
-                    path = "darwin-amd64/" .. name,
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://github.com/openservicemesh/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-v" .. version .. "-darwin-amd64.zip",
+            sha256 = "a567ed1a9707787f7ed8035b4a602d16491823462fa8ef8660cd21f5cd34b406",
+            resources = {
+                {
+                    path = name,
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -24,12 +37,24 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/openservicemesh/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
-            -- shasum of the release archive
-            sha256 = "541dc7b9efc62268192e4a92155842d1dd8f3ae22953274da364d921ff99d066",
+            url = "https://github.com/openservicemesh/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
+            sha256 = "158ab320644fa58362ed84c80ff878dcf6810ca813ba0d9fc8e20ecce74eea7e",
             resources = {
                 {
-                    path = "linux-amd64/" .. name,
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://github.com/openservicemesh/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-v" .. version .. "-linux-amd64.zip",
+            sha256 = "2e15a7a7e95dd0542436b0d1825427b0d875d93aec2f366bd365b587e612b866",
+            resources = {
+                {
+                    path = name,
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -38,12 +63,23 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/openservicemesh/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "windows-amd64.zip",
-            -- shasum of the release archive
-            sha256 = "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
+            url = "https://github.com/openservicemesh/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-v" .. version .. "-windows-amd64.tar.gz",
+            sha256 = "f9d8e073506143b5d19943ad0c358b39f1b698954300c682f5b26c1cb85b780c",
             resources = {
                 {
-                    path = "windows-amd64" .. name .. ".exe",
+                    path = name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://github.com/openservicemesh/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-v" .. version .. "-windows-amd64.zip",
+            sha256 = "16d379c00aeae4dce7f19768ebdfb3d5f5f0e754ea9ef45b73280eed521bd49b",
+            resources = {
+                {
+                    path = name .. ".exe",
                     installpath = "bin\\" .. name .. ".exe"
                 }
             }
