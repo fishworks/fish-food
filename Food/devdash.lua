b645f77d44d330e6f2bffb8058c55fc3e0960bd0
Dylan Arbour <arbourd@users.noreply.github.com>
2021-02-28 13:29:33 -0500

    devdash 0.4.2 fix
    Looks like it was re-released to remove this:
    https://github.com/Phantas0s/devdash/commit/2dea2ffed90acc677e5dad04cd0aaf467c11177e

--- Food/devdash.lua
+++ Food/devdash.lua
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/Phantas0s/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_Darwin_x86_64.tar.gz",
-            sha256 = "fddf7f53758182438a52426af27901b0251806488bab7eb94b09d579ccb621b7",
+            sha256 = "ff1bee3f234bc2bfe85f6bd594b38f73fdaddb656a0a1433bca0a42b5f0e86ab",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/Phantas0s/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_Linux_x86_64.tar.gz",
-            sha256 = "831c4542a78629a0bc95af48c0948cf4497772effb64c74c882191bc28acdc09",
+            sha256 = "889f37759e9658c638630bba0e7821e7c430da3b5663691dea7fa04f1f4ee7ef",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/Phantas0s/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_Windows_x86_64.zip",
-            sha256 = "9b9f1b859e8eb4c8be7af270b53a06aed885345100ef6635a150e0fe22552be5",
+            sha256 = "d94da8682c850f4f683b5dcd704fe7083683438d465e5d3dacc72491c3ee7acb",
             resources = {
                 {
                     path = name .. ".exe",
