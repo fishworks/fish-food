a9586660c0038f2e695590746d11468773d2beef
Frederik Mogensen <fmotrifork>
2021-03-02 15:52:27 +0100

    scorecard 1.1.1

---
+++ Food/scorecard.lua
@@ -0,0 +1,12 @@
+local name = "scorecard"
+local release = "v1.1.1"
+local version = "1.1.1"
+food = {
+    name = name,
+    description = "Security Scorecards - Security health metrics for Open Source",
+    license = "Apache-2.0",
+    homepage = "https://github.com/ossf/scorecard",
+    version = version,
+    packages = {
+    }
+}
