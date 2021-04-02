25f091dc99489cb19af4f2c6025f1c0c689f585e
Dylan Arbour <arbourd@users.noreply.github.com>
2021-04-02 11:47:39 -0400

    flux 0.11.0

--- Food/flux.lua
+++ Food/flux.lua
@@ -1,5 +1,5 @@
 local name = "flux"
-local version = "0.9.0"
+local version = "0.11.0"
 local release = "v" .. version
 
 food = {
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/fluxcd/flux2/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_darwin_amd64.tar.gz",
-            sha256 = "09f8e80e86e596a3f226179e5a4bbfc50cc858d93c749920c49318ba6dc945d6",
+            sha256 = "13301d745b6fb7aef00c2d2d33df1bc718ceabde4a1b574702e0c393d5962f7a",
             resources = {
                 {
                     path = name,
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/fluxcd/flux2/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_linux_amd64.tar.gz",
-            sha256 = "bf0a20749b339f70e464aab0554db055358b43cb174a8b1d40397d1ea448c145",
+            sha256 = "6ef6f7ea98f5ba04d71d1490690ecffc5b2b1457b287ae32d0174c2e53d150aa",
             resources = {
                 {
                     path = name,
@@ -39,7 +39,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/fluxcd/flux2/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_windows_amd64.zip",
-            sha256 = "b451cf1b71d294b3b49274901375b7797dd44e140927024b6cbd3b9c5a1fd9a7",
+            sha256 = "0ffd8159d02878c4b740afff985f97eddbedb069d25cb3a5befc1f8aa1a9acd6",
             resources = {
                 {
                     path = name .. ".exe",
