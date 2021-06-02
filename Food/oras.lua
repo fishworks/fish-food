11c6e9c1c158a47c6ac42ca7d3e8d4ad44566dd3
Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>
2021-06-01 22:09:19 -0400

    oras v0.12.0

--- Food/oras.lua
+++ Food/oras.lua
@@ -1,19 +1,19 @@
 local name = "oras"
-local version = "0.11.1"
+local version = "0.12.0"
 local release = "v" .. version
 
 food = {
     name = name,
     description = "OCI Registry As Storage",
     license = "MIT",
-    homepage = "https://github.com/deislabs/oras",
+    homepage = "https://oras.land",
     version = version,
     packages = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/deislabs/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_darwin_amd64.tar.gz",
-            sha256 = "6441a5075bd9d9c1c841f4d7166dd7d3a22253839c8564ef11b49e969dccd4c8",
+            url = "https://github.com/oras-project/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_darwin_amd64.tar.gz",
+            sha256 = "3637530cd3d01e2b3dc43fc4692edd36c71919726be9fdbb3b298ce0979bbabb",
             resources = {
                 {
                     path = name,
@@ -25,8 +25,8 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/deislabs/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_linux_amd64.tar.gz",
-            sha256 = "ea148379f4fd5fb8e545b92b4b9f8ac1ec3380498986c39c169baddef6b421f9",
+            url = "https://github.com/oras-project/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_linux_amd64.tar.gz",
+            sha256 = "660a4ecd87414d1f29610b2ed4630482f1f0d104431576d37e59752c27de37ed",
             resources = {
                 {
                     path = name,
@@ -38,8 +38,8 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/deislabs/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_windows_amd64.tar.gz",
-            sha256 = "fa5a3753ef0894530eb986db258ca63b7c485e2170dcba5ace8b56f7417d2e62",
+            url = "https://github.com/oras-project/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. version .. "_windows_amd64.tar.gz",
+            sha256 = "bdd9a3a7fa014d0f2676fed72bba90710cd80c1ae49e73a5bfcc944ee0ac4505",
             resources = {
                 {
                     path = name .. ".exe",
