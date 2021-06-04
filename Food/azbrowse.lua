efaffcaf43ed4d73a5def71b3aaf5f1f4afb7e17
Frederik Mogensen <sodiaf@gmail.com>
2021-06-04 13:59:20 +0200

    azbrowse 2.0.221983990
    - Remove assets that are no longer released.
    - Fix archive type on windows releases
    - Upgrade version to newes

--- Food/azbrowse.lua
+++ Food/azbrowse.lua
@@ -1,5 +1,5 @@
 local name = "azbrowse"
-local version = "2.0.221983990"
+local version = "2.0.885432590"
 local repo = "lawrencegripper"
 
 food = {
@@ -8,13 +8,12 @@ food = {
     homepage = "https://github.com/lawrencegripper/azbrowse",
     version = version,
     packages = {
--- https://github.com/lawrencegripper/azbrowse/releases/download/v2.0.221983990/azbrowse_linux_amd64.tar.gz
         {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/" .. repo .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_linux_amd64.tar.gz",
             -- shasum of the release archive
-            sha256 = "2dbcef346d3ba74ac4394a077174e900bda6fbfd89e26cf31aaeb2de6b152db4",
+            sha256 = "acf84a9c8852014953758abba9d80b3db51118619051a84c739b099aeeb2df9e",
             resources = {
                 {
                     path = name,
@@ -28,7 +27,7 @@ food = {
             arch = "386",
             url = "https://github.com/" .. repo .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_linux_386.tar.gz",
             -- shasum of the release archive
-            sha256 = "a5916a395a4465a8f3bfed63536705479c48c793b5d7def9d63e208f500e3c98",
+            sha256 = "240fb96807f9a58bd9f1556ecc3a5995238eeabb8595bd08199edd0787ab0c67",
             resources = {
                 {
                     path = name,
@@ -42,21 +41,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/" .. repo .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_darwin_amd64.tar.gz",
             -- shasum of the release archive
-            sha256 = "bf84178a9ebcda69b5d072415dcbd22a63f2247c2132989f8204448ce56c3b4f",
-            resources = {
-                {
-                    path = name,
-                    installpath = "bin/" .. name,
-                    executable = true
-                }
-            }
-        },
-        {
-            os = "darwin",
-            arch = "386",
-            url = "https://github.com/" .. repo .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_darwin_386.tar.gz",
-            -- shasum of the release archive
-            sha256 = "8b1e2cbac36d7c7f4cf1c918b3b221d258cf1cf2b17ebed7c13b89d80f33407c",
+            sha256 = "98e6f2cfcabb784781ad1e7f5250a6fc1d67057e96aace68037a55a4b77ab6bc",
             resources = {
                 {
                     path = name,
@@ -68,9 +53,9 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/" .. repo .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_windows_amd64.tar.gz",
+            url = "https://github.com/" .. repo .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_windows_amd64.zip",
             -- shasum of the release archive
-            sha256 = "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
+            sha256 = "a2569e6859b7c38da73b1512828e3e0f21defa097768b4248016312407a393de",
             resources = {
                 {
                     path = name .. ".exe",
@@ -81,9 +66,9 @@ food = {
         {
             os = "windows",
             arch = "386",
-            url = "https://github.com/" .. repo .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_windows_386.tar.gz",
+            url = "https://github.com/" .. repo .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_windows_386.zip",
             -- shasum of the release archive
-            sha256 = "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
+            sha256 = "be1f5b5e45b84ca885bc43f6d03eb50ddd935c03e1acc2254e065c0aef760495",
             resources = {
                 {
                     path = name .. ".exe",
