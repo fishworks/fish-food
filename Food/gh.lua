295cc264612d05a555f0bcbb5aea73a0e1f39cad
Masaki Muranaka <monaka@monami-ya.com>
2021-09-18 10:16:06 +0900

    Update gh to 2.0.0.
    Signed-off-by: Masaki Muranaka <monaka@monami-ya.com>

--- Food/gh.lua
+++ Food/gh.lua
@@ -1,5 +1,5 @@
 local name = "gh"
-local version = "1.14.0"
+local version = "2.0.0"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/cli/cli/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_macOS_amd64.tar.gz",
             -- shasum of the release archive
-            sha256 = "dbedd49ba237f25538eb30456cf3048518ac52dacb9496263774637aa1c9eccd",
+            sha256 = "33dd2c9aff1ef2397100263e6c63d4f2a119add725eb4fb10d684412879e6d0a",
             resources = {
                 {
                     path = name .. "_" .. version .. "_macOS_amd64" .. "/bin/" .. name,
@@ -26,7 +26,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/cli/cli/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_linux_amd64.tar.gz",
             -- shasum of the release archive
-            sha256 = "2d175e2b471984baf7ec5ba43175f16addb331d0d15caebea5d510a04911de78",
+            sha256 = "20c2d1b1915a0ff154df453576d9e97aab709ad4b236ce8313435b8b96d31e5c",
             resources = {
                 {
                     path = name .. "_" .. version .. "_linux_amd64" .. "/bin/" .. name,
@@ -40,7 +40,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/cli/cli/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_windows_amd64.zip",
             -- shasum of the release archive
-            sha256 = "6d47bfceb547fe60ad09528ce445aff8cc248653e6ce457b154f705ccced790f",
+            sha256 = "3c33ac49e2f969cc76d082a88db13f6c8050a5677ff149bce7adaca06a2b81c6",
             resources = {
                 {
                     path = "bin/" .. name .. ".exe",
