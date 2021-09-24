63ce5e5447c733c952025054fab8361403036d33
Dave Henderson <dhenderson@gmail.com>
2021-09-24 17:12:14 -0400

    gomplate: upgrade to 3.10.0
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- Food/gomplate.lua
+++ Food/gomplate.lua
@@ -1,5 +1,5 @@
 local name = "gomplate"
-local version = "3.9.0"
+local version = "3.10.0"
 
 food = {
     name = name,
@@ -13,7 +13,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/hairyhenderson/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_darwin-amd64-slim",
             -- shasum of the release archive
-            sha256 = "62473968cb7cc219be24f1008d1384fdc55c42df3c00952a9316285d7d24c62c",
+            sha256 = "5162378d0d3cdecfb2d3a95f0e454e46ee3f185f6415504a555ace9ce0cd7ef4",
             resources = {
                 {
                     path = name .. "_darwin-amd64-slim",
@@ -27,7 +27,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/hairyhenderson/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_linux-amd64-slim",
             -- shasum of the release archive
-            sha256 = "6a780f3c9fa55fa583833322b522e14a19f857f4a4058af1a65fac21b342447a",
+            sha256 = "603539aac4e09f98a8ca5b6e5da0c21213221206dc7175a5644255c7a22b936d",
             resources = {
                 {
                     path = name .. "_linux-amd64-slim",
@@ -41,7 +41,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/hairyhenderson/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_windows-amd64-slim" .. ".exe",
             -- shasum of the release archive
-            sha256 = "bba137242a67ee5ee31ddebf4778f92e7be353109307452f8e663912f14b8e8b",
+            sha256 = "c6625f8e2b0e0476422a075e78704d68fa84cba856bc04aba76ebe00def60804",
             resources = {
                 {
                     path = name .. "_windows-amd64-slim" .. ".exe",
