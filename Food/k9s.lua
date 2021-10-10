d0ef16e81d0b2749ce2429302ed650cde9e4550b
Dylan Arbour <arbourd@users.noreply.github.com>
2021-10-10 15:48:38 -0400

    k9s 0.24.15

--- Food/k9s.lua
+++ Food/k9s.lua
@@ -1,19 +1,19 @@
 local name = "k9s"
-local version = "0.24.10"
+local version = "0.24.15"
 local release = "v" .. version
 
 food = {
     name = name,
     description = "🐶 Kubernetes CLI To Manage Your Clusters In Style!",
-    license = "NOASSERTION",
+    license = "Apache-2.0",
     homepage = "https://github.com/derailed/k9s",
     version = version,
     packages = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/derailed/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. release .. "_Darwin_x86_64.tar.gz",
-            sha256 = "742fdc53d7e2f3b9bbd78de45b7dc9e453f84f5d3f6e594c057d4d823d8dda42",
+            url = "https://github.com/derailed/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_Darwin_x86_64.tar.gz",
+            sha256 = "ba33b0e0fd0679668b7af85fe95fbdc78cfc4ad4e10512146656ee382f1ce8ae",
             resources = {
                 {
                     path = name,
@@ -25,8 +25,8 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/derailed/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. release .. "_Linux_x86_64.tar.gz",
-            sha256 = "bf1fbcf145b8e021b675bae009183e7028afbce52fc44c37f6a0dd2982152c21",
+            url = "https://github.com/derailed/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_Linux_x86_64.tar.gz",
+            sha256 = "0590c32c20cd22416f2a06fad9155257c156e32c60e8757bf96d5a28cb1720dd",
             resources = {
                 {
                     path = name,
@@ -38,8 +38,8 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/derailed/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_" .. release .. "_Windows_x86_64.tar.gz",
-            sha256 = "d77262593c554c98cb70eb086068d4b0fa025d96f116e37d013069a007b73562",
+            url = "https://github.com/derailed/" .. name .. "/releases/download/" .. release .. "/" .. name .. "_Windows_x86_64.tar.gz",
+            sha256 = "1cbd92c1610af1982d16a0e10d4fbf23cbfb938ee09587949ef9902700c1b792",
             resources = {
                 {
                     path = name .. ".exe",
