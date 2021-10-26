0bd32b72cf10a55fdbcd2149ddf3e25cfd903723
arbourd <arbourd@users.noreply.github.com>
2021-10-26 15:24:26 -0400

    kube-no-trouble 0.5.1

--- Food/kube-no-trouble.lua
+++ Food/kube-no-trouble.lua
@@ -1,6 +1,6 @@
 local name = "kube-no-trouble"
-local release = "0.5.0"
-local version = "0.5.0"
+local release = "0.5.1"
+local version = "0.5.1"
 
 -- Executable name is actually "kubent" (vs. "kube-no-trouble")
 local exe = "kubent"
@@ -16,7 +16,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/doitintl/" .. name .. "/releases/download/" .. release .. "/kubent-" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "02f8f418d2d278f7663c38f3ea71ef55dc2c2c7c04439176ea73f78975dcefa1",
+            sha256 = "451ebcaa3993091629892f9f6a3d67f94d84d3eade4ae5c888735d1a66b918b1",
             resources = {
                 {
                     path = exe,
@@ -29,7 +29,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/doitintl/" .. name .. "/releases/download/" .. release .. "/kubent-" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "60fb3067324fdf12849b9359c0b9519b977fcb4f3b6491bd4054093865f23d6a",
+            sha256 = "5224e608c5c65e1c852ada6e0646e7f73a9f8bcc7c13b182db735895cdfdd3dc",
             resources = {
                 {
                     path = exe,
@@ -42,7 +42,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/doitintl/" .. name .. "/releases/download/" .. release .. "/kubent-" .. version .. "-windows-amd64.tar.gz",
-            sha256 = "da923e68f234efeff560acdb5f78d81480a975fe801a425ccf57250ec1346501",
+            sha256 = "a43fcce84ff1eb201cf92982887f1f6794d12654decd3cd5fd43582d481ed4bf",
             resources = {
                 {
                     path = exe .. ".exe",
