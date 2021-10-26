16778393cc36619ade0c13d611bea86093cf352f
arbourd <arbourd@users.noreply.github.com>
2021-10-26 15:24:19 -0400

    gh 2.2.0

--- Food/gh.lua
+++ Food/gh.lua
@@ -1,5 +1,5 @@
 local name = "gh"
-local version = "2.1.0"
+local version = "2.2.0"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/cli/cli/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_macOS_amd64.tar.gz",
             -- shasum of the release archive
-            sha256 = "258944c59cb34c9e8716ecc1e7a3d90f72da9b96b4d85ec9b7b773b4370c88ff",
+            sha256 = "c9c8a716f79fbd8f0a165292e94550f15d0208e6cd3383e5ce942fd2a9ada57f",
             resources = {
                 {
                     path = name .. "_" .. version .. "_macOS_amd64" .. "/bin/" .. name,
@@ -26,7 +26,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/cli/cli/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_linux_amd64.tar.gz",
             -- shasum of the release archive
-            sha256 = "eddadd0cf7ecf340614dd5914eaad1543d8491c966a3a3a41f8a03832dfac184",
+            sha256 = "76bd37160c61cf668b96a362ebc01d23736ebf94ec9dfe3090cacea37fd3b3fb",
             resources = {
                 {
                     path = name .. "_" .. version .. "_linux_amd64" .. "/bin/" .. name,
@@ -40,7 +40,7 @@ food = {
             arch = "amd64",
             url = "https://github.com/cli/cli/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_windows_amd64.zip",
             -- shasum of the release archive
-            sha256 = "34fa202c7cf731f7ff134535200da92af3d7cde05275c3621a92a3fdd65f6a07",
+            sha256 = "736d3a6090687b1a3d4f98d9cd8344b50bb3c37264e32890936052c3725574c5",
             resources = {
                 {
                     path = "bin/" .. name .. ".exe",
