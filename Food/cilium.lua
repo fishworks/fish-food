35adc019ce6808f0a95f51e0369afd2f50352f16
Dylan Arbour <arbourd@users.noreply.github.com>
2021-12-11 20:19:29 -0500

    cilium 0.10.0

--- Food/cilium.lua
+++ Food/cilium.lua
@@ -1,5 +1,5 @@
 local name = "cilium"
-local version = "0.9.3"
+local version = "0.10.0"
 local release = "v" .. version
 
 food = {
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/cilium/cilium-cli/releases/download/" .. release .. "/" .. name .. "-darwin-amd64.tar.gz",
-            sha256 = "0d76678957759d51f0b36712ae0fa8d45e7c2733723f9e10970696bda0bb757d",
+            sha256 = "eeb4cf81062e0d6297188b6fc0cd4b670fe5c5e46ed3f96fed11b315d5beee19",
             resources = {
                 {
                     path = name,
@@ -26,7 +26,7 @@ food = {
             os = "darwin",
             arch = "arm64",
             url = "https://github.com/cilium/cilium-cli/releases/download/" .. release .. "/" .. name .. "-darwin-arm64.tar.gz",
-            sha256 = "e0e9b881b41a870109ef6368d809b5e8b601901505fec5c3943d4cd9a2c5644e",
+            sha256 = "834650f86339df655af4fc454c9c698472ec116e30b8afb7efb7afd634d9b489",
             resources = {
                 {
                     path = name,
@@ -39,7 +39,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/cilium/cilium-cli/releases/download/" .. release .. "/" .. name .. "-linux-amd64.tar.gz",
-            sha256 = "a36e81739af622a8d8c7f3fdcdcf67a12468c6abeac5d3fa4d93ca2470622cdb",
+            sha256 = "2a309542dc9504d3ee3583617fef98e5cb24cdb5e8578a52fa6584932bb2c46c",
             resources = {
                 {
                     path = name,
@@ -52,7 +52,7 @@ food = {
             os = "linux",
             arch = "arm64",
             url = "https://github.com/cilium/cilium-cli/releases/download/" .. release .. "/" .. name .. "-linux-arm64.tar.gz",
-            sha256 = "1aa5a424bd713161d32e5ed3a1c002120f70ae78a0cd4fce9d251744de75400d",
+            sha256 = "385c440d7bc549c7c3e3beee029591c80dc2aea799251f48fbe8eec3ef1e8e94",
             resources = {
                 {
                     path = name,
@@ -65,7 +65,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/cilium/cilium-cli/releases/download/" .. release .. "/" .. name .. "-windows-amd64.tar.gz",
-            sha256 = "02c0d277cfba71826ba1532b8f567233af33dc45f9c11957bd855d7e6538d510",
+            sha256 = "b52bdc026be19abc034d10b6550062276902255cb21a7451780a912f1738f813",
             resources = {
                 {
                     path = name .. ".exe",
