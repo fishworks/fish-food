36f895128b9adb0572af081de822fb9b57150929
Dylan Arbour <arbourd@users.noreply.github.com>
2022-03-07 13:32:04 -0500

    Add arm64 binaries to act

--- Food/act.lua
+++ Food/act.lua
@@ -22,10 +22,36 @@ food = {
             }
         },
         {
+            os = "darwin",
+            arch = "arm64",
+            url = "https://github.com/nektos/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_Darwin_arm64.tar.gz",
+            sha256 = "1393d9a568d4f711e1a4b98f021a0f49598a628ba6067e36d10bdc1b72b02e88",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/nektos/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_Linux_x86_64.tar.gz",
             sha256 = "738534c2b68e1f41b967bdc15206aac681293b937b37bbfff1b754b98a5615a2",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "arm64",
+            url = "https://github.com/nektos/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_Linux_arm64.tar.gz",
+            sha256 = "690bdd6403249c3c123ba6417d2a8d5943348e00be6371a58f6712dc8b9b91c7",
             resources = {
                 {
                     path = name,
